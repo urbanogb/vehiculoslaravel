621894aa2a301cdd7ed7a632aba3e1043b7e13f7
Gabriel Iban <gabriel.iban@zonafrancavigo.com>
2021-03-22 15:47:37 +0100

    con post de vehiculos funcionando con valores fijos. el test dice que falla pero graba en la Base de Datos y devuelve 201

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -2,65 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\misClases\BDDef;
 use App\Models\vehiculo;
 use App\misClases\StandarResponse;
 use Illuminate\Http\Request;
 
 class VehiculoController extends Controller
 {
-     /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-	public function index()
-	{
-		return StandarResponse::OK(vehiculo::all());
-	}
- /**
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        /*
+    $result = vehiculo::create([
+                "nombreVehiculo" => "Vehiculo test"
+                , "marcaYModelo" => "marcaymodelo"
+                , "matricula" => "sinmat"
+                , "anoCompra" => 2019
+                , "estado" => ""
+                , "observaciones" => "Delectus ut aut autem voluptatibus iusto."
+    ]);
+    */
+        return StandarResponse::OK(vehiculo::all());
+    }
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
-             */
-public function store(Request $request)
-{
-    echo $request;
+     */
+    public function store(Request $request)
+    {
+        echo $request;
 
-    $result = "hola";
-    //$result = vehiculo::create([
-     //           "nombreVehiculo" => "Vehiculo test"
-    //]);
-    
-    return StandarResponse::OK(json_encode($result),201);
-}
- /**
+        //$result = "hola";
+        $result = new vehiculo;
+        $result->nombreVehiculo = $request->nombreVehiculo;
+        $result->marcaYModelo = "pruebaIndex";
+        $result->matricula = "pruebaIndex";
+        $result->anocompra = 2019;
+        $result->estado = BDDef::ev_activo;
+        $result->observaciones = "pruebaIndex";
+        $result->save();
+
+        return response(StandarResponse::OK($result), 201);
+    }
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
-             */
-	public function show($id)
-	{
-		return StandarResponse::OK(vehiculo::find($id));
-	}
- /**
+     */
+    public function show($id)
+    {
+        return StandarResponse::OK(vehiculo::find($id));
+    }
+    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
-             */
-public function update(Request $request, $id)
-{
-}
- /**
+     */
+    public function update(Request $request, $id)
+    {
+    }
+    /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
-             */
-public function destroy($id)
-{
-}
+     */
+    public function destroy($id)
+    {
+    }
 }
