fcc54fd431d8a5ee6bec06a81023e7d923442af4
URBANO GARCIA BARROS <urbanogb@gmail.com>
2021-03-24 09:09:35 +0100

    version con breeze

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('index.html');
-    //return view('welcome');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
